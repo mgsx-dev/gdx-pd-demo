cebc14cfb00089370ed3efe8cdfe19a6a5eb5d11
Germain AUBERT <germain.aubert@gmail.com>
2017-03-19 11:26:07 +0100

    init clean copy from gdx-pd tests

---
+++ desktop/src/net/mgsx/pd/demo/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package net.mgsx.pd.demo;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import net.mgsx.pd.GdxPdDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		new LwjglApplication(new GdxPdDemo(), config);
+	}
+}
---
+++ desktop/src/net/mgsx/pd/demo/DesktopRemoteLauncher.java
@@ -0,0 +1,18 @@
+package net.mgsx.pd.demo;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import net.mgsx.pd.GdxPdDemo;
+import net.mgsx.pd.PdConfiguration;
+
+public class DesktopRemoteLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration.disableAudio = true;
+		PdConfiguration.remoteEnabled = true;
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		new LwjglApplication(new GdxPdDemo(), config);
+	}
+}
